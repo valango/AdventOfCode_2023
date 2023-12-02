33ff5013a535e1eccc6334297599eb04f4efec82
Villem Alango <valango@users.noreply.github.com>
2023-12-02 08:02:28 +0200

    day02

--- README.md
+++ README.md
@@ -4,13 +4,16 @@ My [adventofcode.com](https://adventofcode.com) adventures.<br />
 
 ## Diary
 
-* `Day 01` **Trebuchet?!**: The part #2 had quite a clever trap in it, making initial _"common sense"_ 😁 left-to-right approach fail.
+* `Day 01` **Trebuchet?!**: The part #2 had quite a clever trap in it, making initial _"common sense"_ 😁 left-to-right
+  approach fail.
+* `Day 02` **Cube Conundrum**: Very simple once you understand the task. I am a slow reader, though.
 
 ## Track record
 
 | day | lines | mins1 | mins2 | M1_µs | M2_µs | D1_µs | D2_µs |
 |----:|------:|------:|------:|------:|------:|------:|------:|
 |  01 |    74 |    15 |    49 |  1035 |  2583 |    11 |    33 |
+|  02 |    71 |    32 |    13 |   192 |   509 |    12 |    12 |
 
 The puzzle #2 time is often shorter, because the bulk of computations often happens during the #1 already.
 
@@ -18,6 +21,7 @@ The puzzle #2 time is often shorter, because the bulk of computations often happ
 
 ## The older ones
 
-* [2021](https://github.com/valango/adventOfCode_2021);
-* [2020](https://github.com/valango/adventOfCode);
-* [2015](https://github.com/valango/AdventOfCode_2015).
+[2022](https://github.com/valango/adventOfCode_2022)
+[2021](https://github.com/valango/adventOfCode_2021)
+[2020](https://github.com/valango/adventOfCode)
+[2015](https://github.com/valango/AdventOfCode_2015)
---
+++ day02.js
@@ -0,0 +1,70 @@
+'use strict'
+// --- Day 2: Cube Conundrum ---
+
+const {assert, loadData, parseInt} = require('./utils')
+const rawInput = [loadData(module.filename), undefined, undefined, undefined]
+
+/** @typedef {{string, number}[]} TData */
+
+/** @returns {TData[]} */
+const build = (data) =>
+  data.map((line, lineIndex) => {
+    const parts = line.slice(line.indexOf(':') + 2).split('; ')
+
+    return parts.map(str => {
+      const quotes = {}, statements = str.split(', ')
+      for (const st of statements) {
+        const r = assert(/(\d+)\s(\S+)/.exec(st), 'Error at line', lineIndex)
+        quotes[r[2]] = parseInt(r[1])
+      }
+      return quotes
+    })
+  })
+
+const parse = (dsn) => {
+  let data = rawInput[dsn]
+
+  if (data && (data = data.split('\n').filter(v => Boolean(v))).length) data = build(data)
+
+  return data
+}
+
+const isPossible = (set, limits) => Object.entries(set).every(([k, v]) => limits[k] >= v)
+
+/** @param {TData[]} input */
+const puzzle1 = (input) => {
+  const limits = {red: 12, green: 13, blue: 14}
+  let sum = 0
+
+  for (let i = 0, game; (game = input[i++]);) {
+    if (game.every(part => isPossible(part, limits))) sum += i
+  }
+  return sum
+}
+
+/** @param {TData} game */
+const power = (game) => {
+  const res = {blue: 0, green: 0, red: 0}
+
+  for (const part of game) {
+    for (const [k, v] of Object.entries(part)) {
+      res[k] = Math.max(v, res[k])
+    }
+  }
+  return Object.values(res).reduce((a, v) => a * v, 1)
+}
+
+/** @param {TData[]} input */
+const puzzle2 = (input) => {
+  return input.reduce((a, game) => a + power(game), 0)
+}
+
+//  Example (demo) data.
+rawInput[1] = `
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
+
+module.exports = {parse, puzzles: [puzzle1, puzzle2]}
