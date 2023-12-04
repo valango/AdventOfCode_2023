810829a65759d38e2e0371ad008ef4f4583ee340
Villem Alango <valango@users.noreply.github.com>
2023-12-04 08:34:18 +0200

    day04.2

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ My [adventofcode.com](https://adventofcode.com) adventures.<br />
 * `Day 02` **Cube Conundrum**: Very simple once you understand the task. I am a slow reader, though.
 * `Day 03` **Gear ratios**: I absolutely HATE _any tasks_ that involve keeping track of _**position indices**_! 🤮
   The parts adjacency checking is too complex, because I expected the parts can be of any length - this did not happen.
+* `Day 04` **Scratchcards**: #1 was super simple; #2 was hard to visualize - a straightforward rules-based simulation 🤖 did the trick!
 
 ## Track record
 
@@ -17,6 +18,7 @@ My [adventofcode.com](https://adventofcode.com) adventures.<br />
 |  01 |    74 |    15 |    49 |  1035 |  2583 |    11 |    33 |
 |  02 |    71 |    32 |    13 |   192 |   509 |    12 |    12 |
 |  03 |    87 |    62 |    36 |  8044 |    16 |    37 |    17 |
+|  04 |    53 |    18 |    66 |   240 |   608 |    12 |    92 |
 
 Up there, 'M' stands for main and 'D' stands for demo data.<br>
 The puzzle #2 time is often shorter, because the bulk of computations may happen during the #1 already.
--- day04.js
+++ day04.js
@@ -1,9 +1,11 @@
 'use strict'
+// --- Day 4: Scratchcards ---
 
-const {assert, getOptions, log, loadData, parseInt} = require('./utils')
+const {loadData} = require('./utils')
 const rawInput = [loadData(module.filename), undefined, undefined, undefined]
+const {floor, min, pow} = Math
 
-/** @typedef {*} TData */
+/** @typedef {string[][]} TData */
 
 const parse = (dsn) => {
   let data = rawInput[dsn]
@@ -13,28 +15,28 @@ const parse = (dsn) => {
       line.slice(line.indexOf(':') + 1).split('|').map(half => half.split(' ').filter(s => s))
     )
   }
-  return data   //  NOTE: The runner will distinguish between undefined and falsy!
+  return data
 }
 
-const getScore = (bets, winning) => {
-  let score = 0
-
-  for (const bet of bets) {
-    if (winning.includes(bet)) {
-      score = score ? score * 2 : 1
-    }
-  }
-  return score
-}
+const getWins = (bets, winning) =>
+  bets.reduce((cnt, bet) => cnt + (winning.includes(bet) ? 1 : 0), 0)
 
 /** @param {TData[]} input */
 const puzzle1 = (input) => {
-  return input.reduce((s, [bets, winning]) => s + getScore(bets, winning), 0)
+  return input.reduce((s, [bets, winning]) => s + floor(pow(2, getWins(bets, winning)) / 2), 0)
 }
 
 /** @param {TData[]} input */
 const puzzle2 = (input) => {
-  return undefined
+  const cards = input.map(([b, w]) => ({factor: getWins(b, w), count: 1}))
+
+  for (let i = 0; i < cards.length; ++i) {
+    for (let lim = min(i + cards[i].factor + 1, cards.length), j = i; ++j < lim;) {
+      cards[j].count += cards[i].count
+    }
+  }
+
+  return cards.reduce((s, c) => s + c.count, 0)
 }
 
 //  Example (demo) data.
@@ -46,7 +48,5 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 `
-//  Uncomment the next line to disable demo for puzzle2 or to define different demo data for it.
-//  rawInput[2] = ``
 
 module.exports = {parse, puzzles: [puzzle1, puzzle2]}
