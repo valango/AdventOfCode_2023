ad53836b0297f78b3d78b26ac7717d6bada5d95e
Villem Alango <valango@users.noreply.github.com>
2023-12-08 10:33:02 +0200

    day08.1 still at demo

--- day08.js
+++ day08.js
@@ -4,13 +4,24 @@
 const {assert, getOptions, log, loadData, parseInt} = require('./utils')
 const rawInput = [loadData(module.filename), undefined, undefined, undefined]
 
-/** @typedef {{lr:number[],map:number[][]}} TData */
+/** @typedef {{lr:number[],map:number[][], start: number}} TData */
+
+/** @param {number[][]} old */
+const optimizeMap = (old) => {
+  const mapping = new Map()
+
+  for (let i = 0; i < old.length; ++i) {
+    mapping.set(old[i][0], i)
+  }
+
+  return old.map(([, l, r]) => ([mapping.get(l), mapping.get(r)]))
+}
 
 const parse = (dsn) => {
-  let data = rawInput[dsn]
+  let data = rawInput[dsn], map = []
 
   if (data && (data = data.split('\n').filter(v => Boolean(v))).length) {
-    const indexMap = new Map(), map = []
+    const indexMap = new Map()
 
     const translate = (str) => {
       let i = indexMap.get(str)
@@ -20,15 +31,17 @@ const parse = (dsn) => {
       }
       return i
     }
-    let lr, r
+    let lr, r, start
     for (const line of data) {
       if (lr === undefined) lr = Array.from(line).map(ch => 'LR'.indexOf(ch))
       else {
         assert((r = /(\w+)\W+(\w+)\W+(\w+)/.exec(line)), 'BAD LINE')
+        if (r[1] === 'AAA') start = map.length
         map.push([translate(r[1]), translate(r[2]), translate(r[3])])
       }
     }
-    data = {lr, map}
+    map = optimizeMap(map)
+    data = {lr, map, start}
   }
   return data
 }
@@ -39,13 +52,12 @@ const traverse = (input) => {
   const {lr, map} = input, dst = map.length - 1, limit = lr.length
   let ip = 0, steps = 0
 
-  for (let dir = 0, inode = 0; ; ++ip, ++steps) {
-    const [v, l, r] = map[inode]
-    if (l === r && l === v) break
+  for (let dir = 0, inode = input.start; ; ++ip, ++steps) {
+    const [l, r] = map[inode]
+    if (l === r && l === inode) break
     if (ip === limit) ip = 0
     dir = lr[ip]
     inode = dir ? r : l
-    inode = map.findIndex(([i]) => i === inode)
   }
 
   return steps
@@ -70,7 +82,7 @@ CCC = (ZZZ, GGG)
 DDD = (DDD, DDD)
 EEE = (EEE, EEE)
 GGG = (GGG, GGG)
-ZZZ = (ZZZ, ZZZ`
+ZZZ = (ZZZ, ZZZ)`
 //  Uncomment the next line to disable demo for puzzle2 or to define different demo data for it.
 rawInput[2] = `LLR
 
